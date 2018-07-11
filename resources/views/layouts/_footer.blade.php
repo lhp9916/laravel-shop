9a683cb990794b62c070abb38c270965da511a4a
刘海朋 <991695066@qq.com>
2018-07-11 17:02:08 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://weibo.com/u/lhp9916" target="_blank">lhp9916</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:lhp9916@qq.com">联系我们</a></p>
+    </div>
+</footer>
