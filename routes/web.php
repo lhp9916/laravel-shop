b98c241ec7de47e81bad4698c9b9f6abb4fe45a2
刘海朋 <991695066@qq.com>
2018-07-13 14:49:05 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,15 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
+    //邮箱验证
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
     Route::group(['middleware' => 'email_verified'], function () {
+
+        //收货地址
+        Route::get('user_address', 'UserAddressesController@index')->name('user_address.index');
 
     });
 });
