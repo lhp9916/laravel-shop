9e14165f0569c303fe1116349a4315d0996a6e73
lhp9916 <lhp9916@gmail.com>
2018-08-12 18:14:06 +0800

    管理后台 - 订单列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,4 +19,7 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+
+    //订单
+    $router->get('orders', 'OrderController@index')->name('admin.orders.index');
 });
