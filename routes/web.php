f94f89c148aa343d1e47f465bc474996cd07dc71
lhp9916 <lhp9916@gmail.com>
2018-08-16 20:21:28 +0800

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('orders/{order}/received', 'OrderController@received')->name('orders.received');
         Route::get('orders/{order}/review', 'OrderController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrderController@sendReview')->name('orders.review.store');
+        Route::post('orders/{order}/apply_refund', 'OrderController@applyRefund')->name('orders.apply_refund');
 
         //支付
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
