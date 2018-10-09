7730e67a17ee0a6c466dadee2f147dc2008ce011
刘海朋 <991695066@qq.com>
2018-10-09 13:12:10 +0800

    管理后台商品控制器封装

---
+++ app/Admin/Controllers/CommonProductsController.php
@@ -0,0 +1,100 @@
+<?php
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Layout\Content;
+use App\Models\Product;
+use Encore\Admin\Grid;
+use Encore\Admin\Form;
+
+abstract class CommonProductsController extends Controller
+{
+    use HasResourceActions;
+
+    abstract public function getProductType();
+
+    public function index(Content $content)
+    {
+        return $content
+        ->header(Product::$typeMap[$this->getProductType()].'列表')
+        ->body($this->grid());
+    }
+
+    public function edit($id, Content $content)
+    {
+        return $content
+        ->header('编辑'.Product::$typeMap[$this->getProductType()])
+        ->body($this->form()->edit($id));
+    }
+
+    public function create(Content $content)
+    {
+        return $content
+        ->header('创建'.Product::$typeMap[$this->getProductType()])
+        ->body($this->form());
+    }
+
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+
+        $grid->model()->where('type', $this->getProductType())->orderBy('id', 'desc');
+
+        $this->customGrid($grid);
+
+        $grid->actions(function ($actions) {
+            $actions->disableView();
+            $actions->disableDelete();
+        });
+
+        $grid->tools(function ($tools) {
+            //禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+
+    // 定义一个抽象方法，各个类控制器实现本方法来控制显示哪些字段
+    abstract protected function customGrid(Grid $grid);
+
+    protected function form()
+    {
+        $form = new Form(new Product());
+
+        $form->hidden('type')->value($this->getProductType());
+        // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+        $form->text('title', '商品名称')->rules('required');
+        $form->select('category_id', '类目')->options(function ($id) {
+            $category = Category::find($id);
+            if ($category) {
+                return [$category->id => $category->full_name];
+            }
+        })->ajax('/admin/api/categories?is_directory=0');
+        $form->image('image', '封面照片')->rules('required|image');
+        //富文本编辑器
+        $form->editor('description', '商品描述')->rules('required');
+        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+
+        $this->customForm();
+
+        $form->hasMany('skus', 'Skus', function (Form\NestedForm $form) {
+            $form->text('title', 'SKU名称')->rules('required');
+            $form->text('description', 'SKU描述')->rules('required');
+            $form->text('price', '单价')->rules('required|numeric|min:0.01');
+            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+        });
+
+        //定义事件回调，当模型保存时会触发这个回调
+        $form->saving(function (Form $form) {
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+        });
+
+        return $form;
+    }
+
+    abstract protected function customForm(Form $form);
+}
