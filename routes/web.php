34fafb7f5b6cd615251446858d0229268c3bb544
lhp9916 <lhp9916@gmail.com>
2018-10-22 22:40:16 +0800

    支付宝分期还款

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
         Route::get('installments', 'InstallmentsController@index')->name('installments.index');
         Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
+        Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
+        Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
 
         //优惠券
         Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
@@ -73,3 +75,5 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
+
+Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
