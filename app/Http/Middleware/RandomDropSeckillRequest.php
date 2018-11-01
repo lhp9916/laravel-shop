993dbb7f4688c7e5a27091195e253040794a2f62
lhp9916 <lhp9916@gmail.com>
2018-11-01 23:29:47 +0800

    随机拒绝

---
+++ app/Http/Middleware/RandomDropSeckillRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\InvalidRequestException;
+use Closure;
+
+class RandomDropSeckillRequest
+{
+    public function handle($request, Closure $next, $percent)
+    {
+        if (random_int(0, 100) < (int)$percent) {
+            throw new InvalidRequestException('参与的用户过多，请稍后再试', 403);
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 // 秒杀
-Route::post('seckill_orders', 'OrderController@seckill')->name('seckill_orders.store');
+Route::post('seckill_orders', 'OrderController@seckill')->name('seckill_orders.store')->middleware('random_drop:80');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 
